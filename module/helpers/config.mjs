ec6175a44b399550d7c0bcd8981c90945feb87f6
Argalad <Eanredur@t-online.de>
2023-08-05 13:51:37 +0200

    Umstellung auf das Boilerplate System als Basis
    Alle Grundlagen ausgetauscht, auch alles umbenannt.

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const AETERNUM = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ AETERNUM.abilities = {
+  "str": "AETERNUM.AbilityStr",
+  "dex": "AETERNUM.AbilityDex",
+  "con": "AETERNUM.AbilityCon",
+  "int": "AETERNUM.AbilityInt",
+  "wis": "AETERNUM.AbilityWis",
+  "cha": "AETERNUM.AbilityCha"
+};
+
+AETERNUM.abilityAbbreviations = {
+  "str": "AETERNUM.AbilityStrAbbr",
+  "dex": "AETERNUM.AbilityDexAbbr",
+  "con": "AETERNUM.AbilityConAbbr",
+  "int": "AETERNUM.AbilityIntAbbr",
+  "wis": "AETERNUM.AbilityWisAbbr",
+  "cha": "AETERNUM.AbilityChaAbbr"
+};
